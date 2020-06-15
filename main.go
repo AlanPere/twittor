b432f616d3b40bc76214d40ca5cb55500db61591
unknown <aalan_666@yahoo.comm>
2020-06-15 18:44:41 -0300

    primero

--- main.go
+++ main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"log"
 
-	"github.com/AlanPere/twittor/bd"
-	"github.com/AlanPere/twittor/handlers"
 )
 
 func main() {
-	if bd.ChequeoConnection() == 0 {
-		log.Fatal("Sin conexión a la BD")
-		return
-	}
-	handlers.Manejadores()
+	
 }
